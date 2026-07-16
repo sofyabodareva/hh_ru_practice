987feba0e0cb0c490bfbe8e4e1277da7e4efb2b1
AizeRs <nakorshunov@mail.ru>
2026-07-17 00:51:33 +0300

    feat: add status logging to tests 2 and 3

--- ui-test/src/test/java/hh/FavoriteVacancyTest.java
+++ ui-test/src/test/java/hh/FavoriteVacancyTest.java
@@ -23,22 +23,33 @@ public class FavoriteVacancyTest extends BaseTest {
     @Test
     @DisplayName("Тест №3: Добавление вакансии в избранное")
     public void testFavoriteVacancy() {
+        logger.info("Тест №3: Добавление вакансии в избранное");
+        logger.info("0) Авторизация");
         MainPage mainPage = new LoginPage().login(hh.utils.Config.getEmail(), hh.utils.Config.getPassword());
 
+        logger.info("1) Нажать на поле поиска вакансии, 2) Ввести название вакансии, 3) Произвести поиск");
         SearchResultsPage searchResultsPage = mainPage.submitSearch();
+        
+        logger.info("Открытие первой найденной карточки вакансии");
         VacancyPage vacancyPage = searchResultsPage.openFirstVacancy();
 
         String vacancyTitle = vacancyPage.getVacancyTitle();
 
+        logger.info("4) Нажать на кнопку «добавить в избранное» в карточке вакансии");
         vacancyPage.clickFavorite();
 
+        logger.info("Дополнительный шаг: скрытие вакансии перед переходом");
         vacancyPage.scrollToTop();
         vacancyPage.clickMoreOptions();
         vacancyPage.clickHideVacancy();
 
+        logger.info("5) Перейти во вкладку избранные вакансии");
         FavoritesPage favoritesPage = vacancyPage.goToFavorites();
 
+        logger.info("6) Проверить наличие добавленной вакансии");
         String errorMessage = String.format(VACANCY_NOT_FOUND_ERR, vacancyTitle);
         Assertions.assertTrue(favoritesPage.isVacancyInFavorites(vacancyTitle), errorMessage);
+        
+        logger.info("Тест №3: Добавление вакансии в избранное завершён успешно");
     }
 }
--- ui-test/src/test/java/hh/HideCompanyTest.java
+++ ui-test/src/test/java/hh/HideCompanyTest.java
@@ -21,24 +21,37 @@ public class HideCompanyTest extends BaseTest {
      * вакансии).
      */
     @Test
-    @DisplayName("Тест №4: Скрытие вакансий компании")
+    @DisplayName("Тест №2: Скрытие вакансий компании")
     public void testHideCompany() {
+        logger.info("Тест №2: Скрытие вакансий компании");
+        logger.info("0) Авторизация и поиск вакансии");
         MainPage mainPage = new LoginPage().login(hh.utils.Config.getEmail(), hh.utils.Config.getPassword());
         SearchResultsPage searchResultsPage = mainPage.submitSearch();
+        
+        logger.info("1) Открыть карточку вакансии");
         VacancyPage vacancyPage = searchResultsPage.openFirstVacancy();
 
         String companyName = vacancyPage.getCompanyName();
         String companyId = vacancyPage.getCompanyId();
 
+        logger.info("2) Нажать кнопку «ещё»");
         vacancyPage.clickMoreOptions();
+        
+        logger.info("3) Нажать кнопку «скрыть вакансии компании»");
         vacancyPage.clickHideCompany();
 
         vacancyPage.closeNotificationIfExists();
 
+        logger.info("4) Открыть меню, 5) Нажать на кнопку «скрытые вакансии и компании»");
         BlacklistPage blacklistPage = vacancyPage.goToBlacklist();
+        
+        logger.info("6) Нажать кнопку «компании»");
         blacklistPage.clickCompaniesTab();
 
+        logger.info("7) Проверить, есть ли скрытая компания в списке");
         String expectedMessage = String.format(COMPANY_NOT_FOUND_MSG, companyName);
         Assertions.assertTrue(blacklistPage.isCompanyInBlacklist(companyId), expectedMessage);
+        
+        logger.info("Тест №2: Скрытие вакансий компании завершён успешно");
     }
 }
