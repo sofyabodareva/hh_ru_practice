629d3e559690bb57f1c1cf14c206f8d6b39f83ee
Windrat <julialev1206@gmail.com>
2026-07-16 03:37:04 +0300

    return old base test

--- ui-test/src/test/java/hh/BaseTest.java
+++ ui-test/src/test/java/hh/BaseTest.java
@@ -1,20 +1,12 @@
 package hh;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 
-import hh.pages.MainPage;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-
-import hh.pages.LoginPage;
-
 public abstract class BaseTest {
-
-    private static final String LOGIN = System.getProperty("hh.login", System.getenv("HH_LOGIN"));
-    private static final String PASSWORD = System.getProperty("hh.password", System.getenv("HH_PASSWORD"));
 
     // Метод, который сработает до тестов
     @BeforeAll
@@ -24,15 +16,6 @@ public abstract class BaseTest {
         Configuration.pageLoadStrategy = "eager";
         
         
-    }
-
-    @BeforeEach
-    public void login() {
-        MainPage mainPage = MainPage.open();
-        LoginPage loginPage = mainPage.clickLogin();
-        loginPage.fillLogin((LOGIN));
-        loginPage.fillPassword(PASSWORD);
-        loginPage.submit();
     }
 
     // Метод, который сработает после тестов 
