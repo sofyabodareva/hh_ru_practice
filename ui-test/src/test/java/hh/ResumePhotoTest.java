bb09b886e3a7ff2b5af935db68415f452c39147f
Windrat <julialev1206@gmail.com>
2026-07-16 06:04:08 +0300

    fixed test 4 and 7

--- ui-test/src/test/java/hh/ResumePhotoTest.java
+++ ui-test/src/test/java/hh/ResumePhotoTest.java
@@ -1,32 +1,36 @@
 package hh;
 
-import hh.pages.ResumeEditPage;
-import hh.pages.MainPage;
-import hh.pages.ProfilePage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import hh.pages.LoginPage;
+import hh.pages.MainPage;
+import hh.pages.ProfilePage;
+import hh.pages.ResumeEditPage;
+
+
 public class ResumePhotoTest extends BaseTest {
 
-    private static final String PHOTO_FILE_PATH = "src/test/resources/photo.jpg";
+    private static final String PHOTO_FILE_PATH = "src/test/resources/cat.jpg";
     private static final String PHOTO_NOT_ADDED_MESSAGE = "Фотография не была добавлена в резюме";
 
     /**
      * Тест №3: Добавление фотографии в резюме.
      * Проверяет, что после загрузки и подтверждения фотография
      * отображается в резюме.
-     * <p>
+     * 
      * Шаги: открыть список резюме в профиле -> открыть резюме на
      * редактирование -> открыть окно загрузки фотографии -> выбрать файл
      * фотографии -> подтвердить выбор -> проверить, что фотография
      * добавлена.
-     * <p>
+     * 
      * Ожидаемый результат: фотография добавлена в резюме.
      */
     @Test
     @DisplayName("Тест №3: Добавление фотографии в резюме")
     public void testAddPhotoToResume() {
+        new LoginPage().login("julialev1206@gmail.com", "Buldak322");
         MainPage mainPage = MainPage.open();
 
         ProfilePage profilePage = mainPage.clickResumesAndProfile();
--- ui-test/src/test/java/hh/SearchFilterTest.java
+++ ui-test/src/test/java/hh/SearchFilterTest.java
@@ -1,32 +1,38 @@
 package hh;
 
-import hh.pages.MainPage;
-import hh.pages.SearchResultsPage;
-import hh.pages.VacancyPage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import hh.pages.LoginPage;
+import hh.pages.MainPage;
+import hh.pages.SearchResultsPage;
+import hh.pages.VacancyPage;
+
+
+
+
 public class SearchFilterTest extends BaseTest {
 
-    private static final String EXPECTED_EXPERIENCE = "не опыта";
+    private static final String EXPECTED_EXPERIENCE = "не требуется";
     private static final String EXPERIENCE_MISMATCH_MESSAGE = "Требуемый опыт вакансии не соответствует выбранному фильтру";
 
     /**
      * Тест №2: Поиск по фильтрам.
      * Проверяет, что вакансия, открытая из результатов поиска, соответствует
      * применённому фильтру по опыту работы.
-     * <p>
+     * 
      * Шаги: нажать на поле поиска -> ввести запрос в поле поиска вакансий ->
      * открыть панель фильтров -> выбрать фильтр по опыту работы -> показать
      * отфильтрованные вакансии -> открыть первую вакансию из списка ->
      * сравнить требуемый опыт вакансии с выбранным фильтром.
-     * <p>
+     * 
      * Ожидаемый результат: требования вакансии соответствуют выбранным фильтрам.
      */
     @Test
     @DisplayName("Тест №2: Поиск по фильтрам")
     public void testSearchByFilters() {
+        new LoginPage().login("julialev1206@gmail.com", "Buldak322");
         MainPage mainPage = MainPage.open();
 
         mainPage.openFiltersPanel();
@@ -34,7 +40,6 @@ public class SearchFilterTest extends BaseTest {
 
         SearchResultsPage searchResultsPage = mainPage.showFilteredVacancies();
         VacancyPage vacancyPage = searchResultsPage.openFirstVacancy();
-
         Assertions.assertEquals(EXPECTED_EXPERIENCE, vacancyPage.getExperienceText(), EXPERIENCE_MISMATCH_MESSAGE);
     }
 }
