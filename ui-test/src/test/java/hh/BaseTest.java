6527439983276f58923640ebac790f797d0278a1
sofyabodareva <bodareva03@gmail.com>
2026-07-16 22:26:54 +0300

    логирование

--- ui-test/src/test/java/hh/BaseTest.java
+++ ui-test/src/test/java/hh/BaseTest.java
@@ -6,11 +6,16 @@ import org.junit.jupiter.api.BeforeAll;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public abstract class BaseTest {
 
+    protected static final Logger logger = LogManager.getLogger(BaseTest.class);
     // Метод, который сработает до тестов
     @BeforeAll
     public static void openBrowser() {
+        logger.info("Настройка конфигурации браузера");
         Configuration.baseUrl = "https://hh.ru";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "normal";
@@ -19,12 +24,14 @@ public abstract class BaseTest {
     // Метод, который сработает перед каждым тестом
     @org.junit.jupiter.api.BeforeEach
     public void setup() {
+        logger.info("Переход на страницу входа");
         Selenide.open("/account/login");
     }
 
     // Метод, который сработает после тестов 
     @AfterEach
     public void closeBrowser() {
+        logger.info("Закрытие браузера");
         Selenide.clearBrowserCookies();
         Selenide.clearBrowserLocalStorage();
     }
