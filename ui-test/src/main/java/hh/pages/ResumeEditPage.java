a68dfd1aec89bcf6d98aaeb43e290a31b1b9fc59
Windrat <154838727+Windrat@users.noreply.github.com>
2026-07-16 18:18:19 +0300

    Update ResumeEditPage.java

--- ui-test/src/main/java/hh/pages/ResumeEditPage.java
+++ ui-test/src/main/java/hh/pages/ResumeEditPage.java
@@ -15,7 +15,7 @@ public class ResumeEditPage extends BasePage {
     private static final String PHOTO_CONFIRM_BUTTON_DATA_QA = "resume-photo-editor-select";
     private static final String PHOTO_IMAGE_DATA_QA = "resume-avatar";
 
-    private static final int DELAY = 1000;
+    private static final int DELAY = 5000;
 
     private final Button photoButton = Button.byDataQa(PHOTO_BUTTON_DATA_QA);
     private final InputFile photoFileInput = InputFile.byDataQa(PHOTO_FILE_INPUT_DATA_QA);
